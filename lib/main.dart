755696a48cd554bd830c67b4091b0083be2062f3
Callum Moffat <smartercallum@gmail.com>
2019-12-27 22:31:20 -0500

    Fix indenting

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,26 @@ import 'pages/tab.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  	@override
-  	Widget build(BuildContext context) {
-	    return MaterialApp(
-    	  	title: 'Flutter Demo',
-	      	theme: ThemeData(
-		        primarySwatch: Colors.green,
-				textTheme: TextTheme(
-					
-				)
-      		),
-      		home: MyHomePage(title: 'Tab 1'),
-    	);
-  	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          textTheme: TextTheme(
+        
+          )
+        ),
+        home: MyHomePage(title: 'Tab 1'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-  	MyHomePage({Key key, this.title}) : super(key: key);
-  	final String title;
-  	@override
-  	_MyHomePageState createState() => _MyHomePageState();
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -40,14 +40,14 @@ class _MyHomePageState extends State<MyHomePage> {
 		super.initState();
 		provider = Provider4Chan(apiUrl: 'https://a.4cdn.org', imageUrl: 'https://i.4cdn.org', name: '4chan');
 	}
-  	@override
-  	Widget build(BuildContext context) {
-      return Material(
-        child: ImageboardTab(
-          initialSite: provider,
-          initialBoard: 'tv',
-          isInTabletLayout: MediaQuery.of(context).size.width > 700
-        )
-      );
-  	}
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: ImageboardTab(
+        initialSite: provider,
+        initialBoard: 'tv',
+        isInTabletLayout: MediaQuery.of(context).size.width > 700
+      )
+    );
+  }
 }
