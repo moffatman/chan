3ae26ebbb9c8cb788961625929d2d06d4f4c672b
Callum Moffat <smartercallum@gmail.com>
2021-03-02 18:21:42 -0500

    Ensure gallery navbar buttons are white

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -47,28 +47,32 @@ class _GalleryPageState extends State<GalleryPage> {
 			slidePageBackgroundHandler: (offset, size) {
 				return Colors.black.withOpacity((0.38 * (1 - (offset.dx / size.width).abs()) * (1 - (offset.dy / size.height).abs())).clamp(0, 1));
 			},
-			child: CupertinoPageScaffold(
-				backgroundColor: Colors.transparent,
-				navigationBar: showChrome ? CupertinoNavigationBar(
-					middle: const Text('Gallery', style: TextStyle(color: Colors.white)),
-					backgroundColor: Colors.black38
-				) : null,
-				child: AttachmentGallery(
-					key: galleryKey,
-					attachments: widget.attachments,
-					initialAttachment: lastAttachment,
-					showThumbnails: showChrome,
-					onTap: (attachment) {
-						setState(() {
-							showChrome = !showChrome;
-						});
-					},
-					onChange: (attachment) {
-						setState(() {
-							lastAttachment = attachment;
-						});
-						widget.onChange?.call(attachment);
-					}
+			child: CupertinoTheme(
+				data: CupertinoThemeData(brightness: Brightness.dark, primaryColor: Colors.white),
+				child: CupertinoPageScaffold(
+					backgroundColor: Colors.transparent,
+					navigationBar: showChrome ? CupertinoNavigationBar(
+						brightness: Brightness.light,
+						middle: Text('Gallery'),
+						backgroundColor: Colors.black38
+					) : null,
+					child: AttachmentGallery(
+						key: galleryKey,
+						attachments: widget.attachments,
+						initialAttachment: lastAttachment,
+						showThumbnails: showChrome,
+						onTap: (attachment) {
+							setState(() {
+								showChrome = !showChrome;
+							});
+						},
+						onChange: (attachment) {
+							setState(() {
+								lastAttachment = attachment;
+							});
+							widget.onChange?.call(attachment);
+						}
+					)
 				)
 			)
 		);
