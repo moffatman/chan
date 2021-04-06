97160b2df9dab9b8a8542200c5ea48f240bc505b
Callum Moffat <smartercallum@gmail.com>
2021-04-05 22:01:18 -0400

    Ensure history tab is showing latest data

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -3,28 +3,35 @@ import 'package:chan/services/persistence.dart';
 import 'package:chan/widgets/thread_row.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:cupertino_back_gesture/src/cupertino_page_route.dart' as cpr;
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HistoryPage extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-		final states = Persistence.threadStateBox.toMap().entries.toList();
-		states.sort((a, b) => b.value.lastOpenedTime.compareTo(a.value.lastOpenedTime));
 		return CupertinoPageScaffold(
 			navigationBar: CupertinoNavigationBar(
 				transitionBetweenRoutes: false,
 				middle: Text('History')
 			),
 			child: Center(
-				child: ListView.builder(
-					itemCount: Persistence.threadStateBox.length,
-					itemBuilder: (context, i) => (states[i].value.thread != null) ? GestureDetector(
-						behavior: HitTestBehavior.opaque,
-						child: ThreadRow(
-							thread: states[i].value.thread!,
-							isSelected: false
-						),
-						onTap: () => Navigator.of(context).push(cpr.CupertinoPageRoute(builder: (ctx) => ThreadPage(board: Persistence.boardBox.get(states[i].value.thread!.board)!, id: states[i].value.thread!.id)))
-					) : Text(states[i].key)
+				child: ValueListenableBuilder(
+					valueListenable: Persistence.threadStateBox.listenable(),
+					builder: (context, Box<PersistentThreadState> box, child) {
+						final states = box.toMap().entries.toList();
+						states.sort((a, b) => b.value.lastOpenedTime.compareTo(a.value.lastOpenedTime));
+						return ListView.builder(
+							itemCount: states.length,
+							itemBuilder: (context, i) => (states[i].value.thread != null) ? GestureDetector(
+								behavior: HitTestBehavior.opaque,
+								child: ThreadRow(
+									thread: states[i].value.thread!,
+									isSelected: false
+								),
+								onTap: () => Navigator.of(context).push(cpr.CupertinoPageRoute(builder: (ctx) => ThreadPage(board: Persistence.boardBox.get(states[i].value.thread!.board)!, id: states[i].value.thread!.id)))
+							) : Text(states[i].key)
+						);
+					}
 				)
 			)
 		);
