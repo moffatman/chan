c473ae31c34c76f79a2928579d82e6db395dfa57
Callum Moffat <smartercallum@gmail.com>
2021-05-27 19:39:04 -0400

    Improve gallery experience within posts modal
    Swipe between each attachment in the modal
    Modal scroll will be synced with current attachment

--- lib/pages/posts.dart
+++ lib/pages/posts.dart
@@ -1,3 +1,4 @@
+import 'package:chan/models/attachment.dart';
 import 'package:chan/models/post.dart';
 import 'package:chan/pages/gallery.dart';
 import 'package:chan/widgets/post_row.dart';
@@ -8,7 +9,7 @@ import 'package:provider/provider.dart';
 import 'package:chan/pages/overscroll_modal.dart';
 import 'package:chan/util.dart';
 
-class PostsPage extends StatelessWidget {
+class PostsPage extends StatefulWidget {
 	final PostSpanZoneData zone;
 	final List<int> postsIdsToShow;
 
@@ -18,35 +19,64 @@ class PostsPage extends StatelessWidget {
 	});
 
 	@override
+	createState() => _PostsPageState();
+}
+
+class _PostsPageState extends State<PostsPage> {
+	Map<Post, BuildContext> postContexts = Map();
+
+	@override
+	void didUpdateWidget(PostsPage old) {
+		super.didUpdateWidget(old);
+		if (widget.zone != old.zone || widget.postsIdsToShow != old.postsIdsToShow) {
+			postContexts = {};
+		}
+	}
+
+	@override
 	Widget build(BuildContext context) {
 		final List<Post> replies = [];
-		for (final id in postsIdsToShow) {
-			final matchingPost = zone.threadPosts.tryFirstWhere((p) => p.id == id);
+		for (final id in widget.postsIdsToShow) {
+			final matchingPost = widget.zone.threadPosts.tryFirstWhere((p) => p.id == id);
 			if (matchingPost != null) {
 				replies.add(matchingPost);
 			}
 			else {
-				final archivedPost = zone.postFromArchive(id);
+				final archivedPost = widget.zone.postFromArchive(id);
 				if (archivedPost != null) {
 					replies.add(archivedPost);
 				}
 			}
 		}
+		final attachments = replies.expand<Attachment>((a) => a.attachment == null ? [] : [a.attachment!]).toList();
 		return OverscrollModalPage(
 			child: ChangeNotifierProvider.value(
-				value: zone,
+				value: widget.zone,
 				child: Builder(
 					builder: (ctx) => ListView(
 						shrinkWrap: true,
 						physics: NeverScrollableScrollPhysics(),
 						children: replies.map((reply) {
-							return PostRow(
-								post: reply,
-								onThumbnailTap: (attachment) {
-									showGallery(
-										context: context,
-										attachments: [attachment],
-										semanticParentIds: ctx.read<PostSpanZoneData>().stackIds
+							return Builder(
+								builder: (context) {
+									postContexts[reply] = context;
+									return PostRow(
+										post: reply,
+										onThumbnailTap: (attachment) {
+											showGallery(
+												context: context,
+												attachments: attachments,
+												initialAttachment: attachment,
+												semanticParentIds: ctx.read<PostSpanZoneData>().stackIds,
+												initiallyShowChrome: true,
+												onChange: (attachment) {
+													final match = postContexts.entries.tryFirstWhere((p) =>  p.key.attachment == attachment);
+													if (match != null) {
+														Scrollable.ensureVisible(match.value, alignment: 0.5, duration: const Duration(milliseconds: 200));
+													}
+												}
+											);
+										}
 									);
 								}
 							);
@@ -54,7 +84,7 @@ class PostsPage extends StatelessWidget {
 					)
 				)
 			),
-			heightEstimate: 100.0 * (postsIdsToShow.length - 1)
+			heightEstimate: 100.0 * (widget.postsIdsToShow.length - 1)
 		);
 	}
 }
