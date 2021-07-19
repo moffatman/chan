eee3a4097e68955f1e456bca35c2624cf764e087
Callum Moffat <smartercallum@gmail.com>
2021-07-19 10:33:02 -0400

    Use GestureDetector for ContextMenu instead of Listener

--- lib/widgets/context_menu.dart
+++ lib/widgets/context_menu.dart
@@ -2,7 +2,6 @@ import 'package:chan/services/settings.dart';
 import 'package:chan/widgets/post_spans.dart';
 import 'package:chan/widgets/util.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -59,68 +58,66 @@ class _ContextMenuState extends State<ContextMenu> {
 			);
 		}
 		else {
-			return Listener(
-				onPointerDown: (event) {
-					if (event.buttons & kSecondaryMouseButton > 0) {
-						final topOfUsableSpace = MediaQuery.of(context).size.height * 0.8;
-						final showOnRight = event.position.dx > (MediaQuery.of(context).size.width - 210);
-						_overlayEntry = OverlayEntry(
-							builder: (context) {
-								return Stack(
-									children: [
-										Positioned.fill(
-											child: GestureDetector(
-												child: Container(color: Colors.transparent),
-												onTap: () => _overlayEntry?.remove(),
-												onSecondaryTap: () => _overlayEntry?.remove()
-											)
-										),
-										Positioned(
-											right: showOnRight ? MediaQuery.of(context).size.width - event.position.dx : null,
-											left: showOnRight ? null : event.position.dx,
-											bottom: (event.position.dy > topOfUsableSpace) ? MediaQuery.of(context).size.height - event.position.dy : null,
-											top: (event.position.dy > topOfUsableSpace) ? null : event.position.dy,
-											width: 200,
-											child: Container(
-												decoration: BoxDecoration(
-													border: Border.all(color: Colors.grey),
-													borderRadius: BorderRadius.all(Radius.circular(4))
-												),
-												child: Column(
-													mainAxisSize: MainAxisSize.min,
-													crossAxisAlignment: CrossAxisAlignment.stretch,
-													children: widget.actions.map((action) {
-														return GestureDetector(
-															child: Container(
-																decoration: BoxDecoration(
-																	color: CupertinoTheme.of(context).scaffoldBackgroundColor,
-																),
-																height: 50,
-																padding: EdgeInsets.all(16),
-																alignment: Alignment.center,
-																child: Row(
-																	children: [
-																		action.child,
-																		Spacer(),
-																		Icon(action.trailingIcon)
-																	]
-																)
+			return GestureDetector(
+				onSecondaryTapUp: (event) {
+					final topOfUsableSpace = MediaQuery.of(context).size.height * 0.8;
+					final showOnRight = event.globalPosition.dx > (MediaQuery.of(context).size.width - 210);
+					_overlayEntry = OverlayEntry(
+						builder: (context) {
+							return Stack(
+								children: [
+									Positioned.fill(
+										child: GestureDetector(
+											child: Container(color: Colors.transparent),
+											onTap: () => _overlayEntry?.remove(),
+											onSecondaryTap: () => _overlayEntry?.remove()
+										)
+									),
+									Positioned(
+										right: showOnRight ? MediaQuery.of(context).size.width - event.globalPosition.dx : null,
+										left: showOnRight ? null : event.globalPosition.dx,
+										bottom: (event.globalPosition.dy > topOfUsableSpace) ? MediaQuery.of(context).size.height - event.globalPosition.dy : null,
+										top: (event.globalPosition.dy > topOfUsableSpace) ? null : event.globalPosition.dy,
+										width: 200,
+										child: Container(
+											decoration: BoxDecoration(
+												border: Border.all(color: Colors.grey),
+												borderRadius: BorderRadius.all(Radius.circular(4))
+											),
+											child: Column(
+												mainAxisSize: MainAxisSize.min,
+												crossAxisAlignment: CrossAxisAlignment.stretch,
+												children: widget.actions.map((action) {
+													return GestureDetector(
+														child: Container(
+															decoration: BoxDecoration(
+																color: CupertinoTheme.of(context).scaffoldBackgroundColor,
 															),
-															onTap: () {
-																action.onPressed();
-																_overlayEntry?.remove();
-															}
-														);
-													}).toList()
-												)
+															height: 50,
+															padding: EdgeInsets.all(16),
+															alignment: Alignment.center,
+															child: Row(
+																children: [
+																	action.child,
+																	Spacer(),
+																	Icon(action.trailingIcon)
+																]
+															)
+														),
+														onTap: () {
+															action.onPressed();
+															_overlayEntry?.remove();
+														}
+													);
+												}).toList()
 											)
 										)
-									]
-								);
-							}
-						);
-						Overlay.of(context, rootOverlay: true)!.insert(_overlayEntry!);
-					}
+									)
+								]
+							);
+						}
+					);
+					Overlay.of(context, rootOverlay: true)!.insert(_overlayEntry!);
 				},
 				child: widget.child
 			);
