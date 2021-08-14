e4e6a139c5d109f674e7e3aa2432ce9561fe744f
Callum Moffat <smartercallum@gmail.com>
2021-08-13 20:17:18 -0400

    Perform image rotation in background thread
    Now it won't block and lag UI

--- lib/services/rotating_image_provider.dart
+++ lib/services/rotating_image_provider.dart
@@ -1,5 +1,6 @@
 
 import 'dart:async';
+import 'dart:isolate';
 import 'dart:typed_data';
 
 import 'package:flutter/rendering.dart';
@@ -21,6 +22,34 @@ class RotatedImageKey {
 	int get hashCode => hashValues(parentKey, quarterTurns);
 }
 
+class _RotateIsolateParam {
+	final Uint8List data;
+	final int quarterTurns;
+	final SendPort sendPort;
+	_RotateIsolateParam(this.data, this.quarterTurns, this.sendPort);
+}
+
+void _rotateImageIsolate(_RotateIsolateParam param) {
+	final originalImage = image.decodeAnimation(param.data)!;
+	final rotatedImage = image.Animation();
+	rotatedImage.frameType = originalImage.frameType;
+	rotatedImage.width = (param.quarterTurns % 2 == 1) ? originalImage.height : originalImage.width;
+	rotatedImage.height = (param.quarterTurns % 2 == 1) ? originalImage.width : originalImage.height;
+	rotatedImage.loopCount = originalImage.loopCount;
+	rotatedImage.backgroundColor = originalImage.backgroundColor;
+	for (final i in originalImage.frames) {
+		final rotatedFrame = image.copyRotate(i, param.quarterTurns * 90, interpolation: image.Interpolation.cubic);
+		rotatedFrame.duration = i.duration;
+		rotatedImage.addFrame(rotatedFrame);
+	}
+	if (rotatedImage.length > 1) {
+		param.sendPort.send(Uint8List.fromList(image.encodeGifAnimation(rotatedImage)!));
+	}
+	else {
+		param.sendPort.send(Uint8List.fromList(image.encodePng(rotatedImage.first)));
+	}
+}
+
 class RotatingImageProvider extends ImageProvider<RotatedImageKey> {
 	final ImageProvider parent;
 	final int quarterTurns;
@@ -32,19 +61,10 @@ class RotatingImageProvider extends ImageProvider<RotatedImageKey> {
 	@override
 	ImageStreamCompleter load(RotatedImageKey key, DecoderCallback decode) {
 		return parent.load(key.parentKey, (data, {bool allowUpscaling = false, int? cacheHeight, int? cacheWidth}) async {
-			final originalImage = image.decodeAnimation(data)!;
-			final rotatedImage = image.Animation();
-			rotatedImage.frameType = originalImage.frameType;
-			rotatedImage.width = (quarterTurns % 2 == 1) ? originalImage.height : originalImage.width;
-			rotatedImage.height = (quarterTurns % 2 == 1) ? originalImage.width : originalImage.height;
-			rotatedImage.loopCount = originalImage.loopCount;
-			rotatedImage.backgroundColor = originalImage.backgroundColor;
-			for (final i in originalImage.frames) {
-				final rotatedFrame = image.copyRotate(i, quarterTurns * 90, interpolation: image.Interpolation.cubic);
-				rotatedFrame.duration = i.duration;
-				rotatedImage.addFrame(rotatedFrame);
-			}
-			return await decode(Uint8List.fromList(rotatedImage.length > 1 ? image.encodeGifAnimation(rotatedImage)! : image.encodePng(rotatedImage.first)), allowUpscaling: allowUpscaling, cacheHeight: cacheHeight, cacheWidth: cacheWidth);
+			final receivePort = ReceivePort();
+			await Isolate.spawn(_rotateImageIsolate, _RotateIsolateParam(data, quarterTurns, receivePort.sendPort));
+			final outData = await receivePort.first as Uint8List;
+			return await decode(outData, allowUpscaling: allowUpscaling, cacheHeight: cacheHeight, cacheWidth: cacheWidth);
 		});
 	}
 
