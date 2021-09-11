9f4574191b4bf6609139fbd46305ed739ab1f089
Callum Moffat <smartercallum@gmail.com>
2021-09-10 21:39:18 -0400

    Update pointer gestures to pointer flow

--- lib/widgets/slider_builder.dart
+++ lib/widgets/slider_builder.dart
@@ -90,9 +90,9 @@ class _SliderBuilderState extends State<SliderBuilder> {
 				_recognizingRecognizer.addPointer(e);
 				_claimingRecognizer.addPointer(e);
 			},
-			onPointerGestureDown: (e) {
-				_recognizingRecognizer.addPointerGesture(e);
-				_claimingRecognizer.addPointerGesture(e);
+			onPointerFlowStart: (e) {
+				_recognizingRecognizer.addPointerFlow(e);
+				_claimingRecognizer.addPointerFlow(e);
 			}
 		);
 	}
