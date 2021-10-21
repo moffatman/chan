db8d18f9df995e72630697001e8d0786801be17c
Callum Moffat <smartercallum@gmail.com>
2021-10-21 02:55:30 -0400

    Fix missing import

--- lib/widgets/cupertino_page_route.dart
+++ lib/widgets/cupertino_page_route.dart
@@ -11,6 +11,7 @@ import 'dart:ui' show lerpDouble;
 import 'package:chan/widgets/weak_gesture_recognizer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
