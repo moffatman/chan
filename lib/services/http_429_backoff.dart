aee5782049c64c68baa9ad556cae8558582fa5c2
Callum Moffat <smartercallum@gmail.com>
2022-02-17 11:34:56 -0500

    Fix 429 backoff

--- lib/services/http_429_backoff.dart
+++ lib/services/http_429_backoff.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class HTTP429BackoffInterceptor extends Interceptor {
 	final Dio client;
 	final int maxRetries;
-	final Map<RequestOptions, int> _retryCount = {};
+	final Map<Uri, int> _retryCount = {};
 
 	HTTP429BackoffInterceptor({
 		required this.client,
@@ -13,11 +13,15 @@ class HTTP429BackoffInterceptor extends Interceptor {
 	@override
 	void onError(DioError err, ErrorInterceptorHandler handler) async {
 		final secondsStr = err.response?.headers.value('retry-after');
-		if (err.type == DioErrorType.response && err.response?.statusCode == 429 && secondsStr != null) {
+		final currentRetries = _retryCount[err.requestOptions.uri] ?? 0;
+		if (err.type == DioErrorType.response &&
+			  err.response?.statusCode == 429 &&
+				secondsStr != null &&
+				currentRetries < maxRetries) {
 			final seconds = int.parse(secondsStr);
-			print('Waiting $seconds seconds due to server-side rate-limiting (current retry count is ${_retryCount[err.requestOptions]}');
+			print('Waiting $seconds seconds due to server-side rate-limiting (current retry count is $currentRetries)');
 			await Future.delayed(Duration(seconds: seconds));
-			_retryCount.update(err.requestOptions, (a) => a + 1, ifAbsent: () => 1);
+			_retryCount[err.requestOptions.uri] = currentRetries + 1;
 			try {
 				final response = await client.requestUri(
 					err.requestOptions.uri,
