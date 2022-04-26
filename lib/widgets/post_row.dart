797da90a6ae9d7fb04025feb1d2403e14598e5cc
Callum Moffat <smartercallum@gmail.com>
2022-04-26 00:11:08 -0400

    Priority of "your post" over "(you)"

--- lib/widgets/post_row.dart
+++ lib/widgets/post_row.dart
@@ -71,7 +71,7 @@ class PostRow extends StatelessWidget {
 				left: BorderSide(color: CupertinoTheme.of(context).textTheme.actionTextStyle.color ?? Colors.red, width: 10)
 			);
 		}
-		if (zone.threadState?.replyIdsToYou(Filter.of(context))?.contains(post.id) ?? false) {
+		else if (zone.threadState?.replyIdsToYou(Filter.of(context))?.contains(post.id) ?? false) {
 			border = Border(
 				left: BorderSide(color: CupertinoTheme.of(context).textTheme.actionTextStyle.color?.towardsBlack(0.5) ?? const Color.fromARGB(255, 90, 30, 30), width: 10)
 			);
