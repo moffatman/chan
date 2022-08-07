5d32a206f754fcbe7d07ac16a33387eb710169be
Callum Moffat <smartercallum@gmail.com>
2022-08-06 21:50:20 -0400

    More reliable MIUI 13 status bar workaround

--- lib/services/status_bar.dart
+++ lib/services/status_bar.dart
@@ -1,12 +1,16 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 
 const _platform = MethodChannel('com.moffatman.chan/statusBar');
 
 Future<void> showStatusBar() async {
 	if (Platform.isIOS) {
 		await _platform.invokeMethod('showStatusBar');
+	}
+	else if (await _workaround()) {
+		await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
 	}
 	else {
 		await SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
@@ -20,6 +24,15 @@ Future<void> hideStatusBar() async {
 		await _platform.invokeMethod('hideStatusBar');
 	}
 	else {
-		await SystemChrome.setEnabledSystemUIMode((Platform.environment['BOOTCLASSPATH'] ?? '').contains('miui-framework') ? SystemUiMode.edgeToEdge : SystemUiMode.immersive);
+		await SystemChrome.setEnabledSystemUIMode(await _workaround() ? SystemUiMode.edgeToEdge : SystemUiMode.immersive);
 	}
+}
+
+Future<bool> _workaround() async {
+	if (!Platform.isAndroid) {
+		return false;
+	}
+	final androidInfo = await DeviceInfoPlugin().androidInfo;
+	final version = int.tryParse(androidInfo.version.release ?? '') ?? 0;
+	return version >= 12 && androidInfo.brand == 'Xiaomi';
 }
