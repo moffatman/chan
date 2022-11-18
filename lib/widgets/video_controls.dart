90519f28e6e811b225cf6cfefebe72b9288f43da
Callum Moffat <smartercallum@gmail.com>
2022-11-17 21:58:35 -0500

    Rewrite video scrubber
    Better looking
    Tap / drag anywhere to scrub
    30hz updates instead of 2hz

--- lib/widgets/video_controls.dart
+++ lib/widgets/video_controls.dart
@@ -1,10 +1,11 @@
-import 'dart:math';
-
 import 'package:chan/services/settings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mutex/mutex.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
+
+const _positionUpdatePeriod = Duration(milliseconds: 30);
 
 class VideoControls extends StatefulWidget {
 	final VideoPlayerController controller;
@@ -21,17 +22,21 @@ class VideoControls extends StatefulWidget {
 }
 
 class _VideoControlsState extends State<VideoControls> {
-	double sliderValue = 0;
 	late VideoPlayerValue value;
 	late bool wasAlreadyPlaying;
+	final position = ValueNotifier(Duration.zero);
+	bool _playingBeforeLongPress = false;
+	bool _currentlyWithinLongPress = false;
+	final _mutex = Mutex();
 
 	@override
 	void initState() {
 		super.initState();
 		value = widget.controller.value;
-		sliderValue = max(0, value.position.inMilliseconds).toDouble();
+		position.value = value.position;
 		wasAlreadyPlaying = value.isPlaying;
 		widget.controller.addListener(_onControllerUpdate);
+		Future.delayed(_positionUpdatePeriod, _updatePosition);
 	}
 
 	@override
@@ -40,19 +45,57 @@ class _VideoControlsState extends State<VideoControls> {
 		if (widget.controller != old.controller) {
 			old.controller.removeListener(_onControllerUpdate);
 			value = widget.controller.value;
-			sliderValue = max(0, value.position.inMilliseconds).toDouble();
 			widget.controller.addListener(_onControllerUpdate);
 		}
 	}
 
 	void _onControllerUpdate() {
+		if (!mounted) {
+			return;
+		}
 		setState(() {
-			if (mounted) {
-				value = widget.controller.value;
-				sliderValue = max(0, value.position.inMilliseconds).toDouble();
-			}
+			value = widget.controller.value;
 		});
 	}
+
+	void _updatePosition() async {
+		if (!mounted) {
+			return;
+		}
+		final newPosition = await widget.controller.position;
+		if (newPosition != null) {
+			position.value = newPosition;
+		}
+		Future.delayed(_positionUpdatePeriod, _updatePosition);
+	}
+
+	Future<void> _onLongPressStart() => _mutex.protect(() async {
+		_playingBeforeLongPress = value.isPlaying;
+		await widget.controller.pause();
+		_currentlyWithinLongPress = true;
+	});
+
+	Future<void> _onLongPressUpdate(double relativePosition) async {
+		if (_currentlyWithinLongPress) {
+			final duration = value.duration.inMilliseconds;
+			final newPosition = Duration(milliseconds: (relativePosition.clamp(0, 1) * (duration)).round());
+			if (!_mutex.isLocked) {
+				await _mutex.protect(() async {
+					await widget.controller.seekTo(newPosition);
+					await widget.controller.play();
+					await widget.controller.pause();
+				});
+			}
+			position.value = newPosition;
+		}
+	}
+
+	Future<void> _onLongPressEnd() => _mutex.protect(() async {
+		if (_playingBeforeLongPress) {
+			await widget.controller.play();
+		}
+		_currentlyWithinLongPress = false;
+	});
 
 	String _formatDuration(Duration d) {
 		return '${d.inMinutes}:${(d.inSeconds % 60).toString().padLeft(2, '0')}';
@@ -60,65 +103,78 @@ class _VideoControlsState extends State<VideoControls> {
 
 	@override
 	Widget build(BuildContext context) {
-		return Row(
-			mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-			children: [
-				const SizedBox(width: 8),
-				Text(_formatDuration(value.position), style: const TextStyle(color: Colors.white)),
-				Expanded(
-					child: CupertinoSlider(
-						value: sliderValue,
-						max: max(0.01, max(sliderValue, value.duration.inMilliseconds).toDouble()),
-						onChangeStart: (newSliderValue) {
-							wasAlreadyPlaying = value.isPlaying;
-							widget.controller.pause();
-						},
-						onChanged: (newSliderValue) {
-							setState(() {
-								sliderValue = newSliderValue;
-							});
-						},
-						onChangeEnd: (newSliderValue) async {
-							await widget.controller.seekTo(Duration(milliseconds: newSliderValue.round()));
-							await widget.controller.play();
-							if (!wasAlreadyPlaying) {
+		return SizedBox(
+			height: 44,
+			child: Row(
+				mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+				children: [
+					const SizedBox(width: 8),
+					ValueListenableBuilder(
+						valueListenable: position,
+						builder: (context, Duration positionValue, _) => Text(_formatDuration(positionValue))
+					),
+					Expanded(
+						child: Padding(
+							padding: const EdgeInsets.all(8),
+							child: LayoutBuilder(
+								builder: (context, constraints) => GestureDetector(
+									onTapUp: (x) async {
+										await _onLongPressStart();
+										await _onLongPressUpdate(x.localPosition.dx / constraints.maxWidth);
+										await _onLongPressEnd();
+									},
+									onHorizontalDragStart: (x) => _onLongPressStart(),
+									onHorizontalDragUpdate: (x) => _onLongPressUpdate(x.localPosition.dx / constraints.maxWidth),
+									onHorizontalDragEnd: (x) => _onLongPressEnd(),
+									child: ClipRRect(
+										borderRadius: BorderRadius.circular(8),
+										child: ValueListenableBuilder(
+											valueListenable: position,
+											builder: (context, Duration positionValue, _) => LinearProgressIndicator(
+												minHeight: 44,
+												value: positionValue.inMilliseconds / value.duration.inMilliseconds,
+												valueColor: AlwaysStoppedAnimation(CupertinoTheme.of(context).primaryColor),
+												backgroundColor: CupertinoTheme.of(context).primaryColor.withOpacity(0.3)
+											)
+										)
+									)
+								)
+							)
+						)
+					),
+					Text(_formatDuration(value.duration), style: const TextStyle(color: Colors.white)),
+					if (widget.hasAudio) AnimatedBuilder(
+						animation: context.read<EffectiveSettings>().muteAudio,
+						builder: (context, _) => CupertinoButton(
+							padding: EdgeInsets.zero,
+							child: Icon(value.volume > 0 ? CupertinoIcons.volume_up : CupertinoIcons.volume_off),
+							onPressed: () async {
+								final settings = context.read<EffectiveSettings>();
+								if (value.volume > 0) {
+									await widget.controller.setVolume(0);
+									settings.setMuteAudio(true);
+								}
+								else {
+									await widget.controller.setVolume(1);
+									settings.setMuteAudio(false);
+								}
+							}
+						)
+					),
+					CupertinoButton(
+						padding: EdgeInsets.zero,
+						child: Icon((_currentlyWithinLongPress ? _playingBeforeLongPress : value.isPlaying) ? CupertinoIcons.pause_fill : CupertinoIcons.play_arrow_solid),
+						onPressed: () async {
+							if (value.isPlaying) {
 								await widget.controller.pause();
 							}
+							else {
+								await widget.controller.play();
+							}
 						},
 					)
-				),
-				Text(_formatDuration(value.duration), style: const TextStyle(color: Colors.white)),
-				if (widget.hasAudio) AnimatedBuilder(
-					animation: context.read<EffectiveSettings>().muteAudio,
-					builder: (context, _) => CupertinoButton(
-						padding: EdgeInsets.zero,
-						child: Icon(value.volume > 0 ? CupertinoIcons.volume_up : CupertinoIcons.volume_off),
-						onPressed: () async {
-							final settings = context.read<EffectiveSettings>();
-							if (value.volume > 0) {
-								await widget.controller.setVolume(0);
-								settings.setMuteAudio(true);
-							}
-							else {
-								await widget.controller.setVolume(1);
-								settings.setMuteAudio(false);
-							}
-						}
-					)
-				),
-				CupertinoButton(
-					padding: EdgeInsets.zero,
-					child: Icon(value.isPlaying ? CupertinoIcons.pause_fill : CupertinoIcons.play_arrow_solid),
-					onPressed: () async {
-						if (value.isPlaying) {
-							await widget.controller.pause();
-						}
-						else {
-							await widget.controller.play();
-						}
-					},
-				)
-			]
+				]
+			)
 		);
 	}
 
