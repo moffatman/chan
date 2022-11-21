57ea7cbecb1826ac404a8af01a99f0367ed31eeb
Callum Moffat <smartercallum@gmail.com>
2022-11-21 17:43:00 -0500

    Allow scrolling horizontal gallery while sheet moves
    Relies on custom branch draggable_sheet_pointer

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -1162,6 +1162,7 @@ class _GalleryPageState extends State<GalleryPage> {
 													maxChildSize: _maxScrollSheetSize,
 													minChildSize: _minScrollSheetSize,
 													controller: _scrollSheetController,
+													shouldIgnorePointer: false,
 													builder: (context, controller) => showChromeOnce ? _buildScrollSheetChild(controller) : const SizedBox.shrink()
 												)
 											)
