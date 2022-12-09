ccb073fc1c11c6a2a6ad8d2291ff2fcac9082d44
Callum Moffat <smartercallum@gmail.com>
2022-12-08 22:14:46 -0500

    Don't cancel pointers during CupertinoContextMenu2

--- lib/widgets/cupertino_context_menu2.dart
+++ lib/widgets/cupertino_context_menu2.dart
@@ -984,6 +984,9 @@ class _ContextMenuRoute<T> extends PopupRoute<T> {
       },
     );
   }
+
+  @override
+  bool get shouldCancelActivePointers => false;
 }
 
 class _ContextMenuRouteStatic extends StatefulWidget {
