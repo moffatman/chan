f4cbb47597a565f45052d72d800a0cb01dd0fe05
Callum Moffat <smartercallum@gmail.com>
2022-12-21 16:10:44 -0500

    More multi-tappable imageboard switcher

--- lib/pages/imageboard_switcher.dart
+++ lib/pages/imageboard_switcher.dart
@@ -1,9 +1,11 @@
 import 'dart:math';
 
 import 'package:chan/services/imageboard.dart';
+import 'package:chan/services/settings.dart';
 import 'package:chan/widgets/imageboard_icon.dart';
 import 'package:chan/widgets/imageboard_scope.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 
 class ImageboardSwitcherPage extends StatefulWidget {
 	final Widget Function(BuildContext context, FocusNode descendantFocusNode) builder;
@@ -24,76 +26,93 @@ class ImageboardSwitcherPage extends StatefulWidget {
 class _ImageboardSwitcherPageState extends State<ImageboardSwitcherPage> {
 	late final PageController _controller;
 	final _focusNodes = <int, FocusNode>{};
+	int _currentPage = 0;
 
 	@override
 	void initState() {
 		super.initState();
+		_currentPage = max(0, ImageboardRegistry.instance.imageboardsIncludingUninitialized.where(widget.filterImageboards ?? (_) => true).toList().indexWhere((b) => b.key == widget.initialImageboardKey));
 		_controller = PageController(
-			initialPage: max(0, ImageboardRegistry.instance.imageboardsIncludingUninitialized.where(widget.filterImageboards ?? (_) => true).toList().indexWhere((b) => b.key == widget.initialImageboardKey))
+			initialPage: _currentPage
 		);
 	}
 
 	@override
 	Widget build(BuildContext context) {
 		final imageboards = ImageboardRegistry.instance.imageboardsIncludingUninitialized.where(widget.filterImageboards ?? (_) => true).toList();
-		return PageView.builder(
-			controller: _controller,
-			itemCount: imageboards.length,
-			onPageChanged: (i) {
-				_focusNodes[i]?.requestFocus();
-			},
-			itemBuilder: (context, i) => ImageboardScope(
-				imageboardKey: null,
-				imageboard: imageboards[i],
-				child: Stack(
-					children: [
-						Builder(
+		return Stack(
+			children: [
+				PageView.builder(
+					controller: _controller,
+					itemCount: imageboards.length,
+					onPageChanged: (i) {
+						_focusNodes[i]?.requestFocus();
+						_currentPage = i;
+					},
+					itemBuilder: (context, i) => ImageboardScope(
+						imageboardKey: null,
+						imageboard: imageboards[i],
+						child: Builder(
 							builder: (context) => widget.builder(context, _focusNodes.putIfAbsent(i, () => FocusNode()))
-						),
-						Positioned.fill(
-							child: Align(
-								alignment: Alignment.bottomCenter,
-								child: Padding(
+						)
+					)
+				),
+				Positioned.fill(
+					child: Align(
+						alignment: Alignment.bottomCenter,
+						child: Container(
+							padding: const EdgeInsets.all(16),
+							width: 250 * context.select<EffectiveSettings, double>((s) => s.textScale),
+							child: AnimatedBuilder(
+								animation: _controller,
+								builder: (context, _) => Container(
+									decoration: BoxDecoration(
+										borderRadius: BorderRadius.circular(16),
+										color: CupertinoTheme.of(context).scaffoldBackgroundColor
+									),
 									padding: const EdgeInsets.all(16),
-									child: Container(
-										decoration: BoxDecoration(
-											borderRadius: BorderRadius.circular(16),
-											color: CupertinoTheme.of(context).scaffoldBackgroundColor
-										),
-										padding: const EdgeInsets.all(16),
-										child: Row(
-											mainAxisSize: MainAxisSize.min,
-											children: [
-												CupertinoButton(
-													padding: EdgeInsets.zero,
-													minSize: 0,
-													onPressed: (i == 0) ? null : () {
-														_controller.animateToPage(i - 1, duration: const Duration(milliseconds: 250), curve: Curves.ease);
-													},
-													child: const Icon(CupertinoIcons.chevron_left)
-												),
-												const SizedBox(width: 8),
-												ImageboardIcon(imageboardKey: imageboards[i].key),
-												const SizedBox(width: 8),
-												Text(imageboards[i].key),
-												const SizedBox(width: 8),
-												CupertinoButton(
-													padding: EdgeInsets.zero,
-													minSize: 0,
-													onPressed: (i + 1 >= imageboards.length) ? null : () {
-														_controller.animateToPage(i + 1, duration: const Duration(milliseconds: 250), curve: Curves.ease);
-													},
-													child: const Icon(CupertinoIcons.chevron_right)
+									child: Row(
+										children: [
+											CupertinoButton(
+												padding: EdgeInsets.zero,
+												minSize: 0,
+												onPressed: (_currentPage == 0) ? null : () {
+													_controller.animateToPage(_currentPage - 1, duration: const Duration(milliseconds: 250), curve: Curves.ease);
+													_currentPage--;
+												},
+												child: const Icon(CupertinoIcons.chevron_left)
+											),
+											const SizedBox(width: 8),
+											Expanded(
+												child: Row(
+													mainAxisAlignment: MainAxisAlignment.center,
+													children: [
+														ImageboardIcon(imageboardKey: imageboards[_currentPage].key),
+														const SizedBox(width: 8),
+														Flexible(
+															child: Text(imageboards[_currentPage].key)
+														),
+													]
 												)
-											]
-										)
+											),
+											const SizedBox(width: 8),
+											CupertinoButton(
+												padding: EdgeInsets.zero,
+												minSize: 0,
+												onPressed: (_currentPage + 1 >= imageboards.length) ? null : () {
+													_controller.animateToPage(_currentPage + 1, duration: const Duration(milliseconds: 250), curve: Curves.ease);
+													_currentPage++;
+												},
+												child: const Icon(CupertinoIcons.chevron_right)
+											)
+										]
 									)
 								)
 							)
 						)
-					]
+					)
 				)
-			)
+			]
 		);
 	}
 
