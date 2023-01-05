74770e12377f04af2e852099deb55229584ddc74
Callum Moffat <smartercallum@gmail.com>
2023-01-05 01:35:42 -0500

    Add Curves.ease to WeakNavigator

--- lib/widgets/weak_navigator.dart
+++ lib/widgets/weak_navigator.dart
@@ -12,9 +12,13 @@ import 'package:tuple/tuple.dart';
 
 class WeakNavigator extends StatefulWidget {
   final Widget child;
+  final Curve curve;
+  final Duration duration;
 
   const WeakNavigator({
     required this.child,
+    this.curve = Curves.ease,
+    this.duration = const Duration(milliseconds: 150),
     Key? key
   }) : super(key: key);
 
@@ -53,10 +57,10 @@ class WeakNavigator extends StatefulWidget {
 }
 
 class WeakNavigatorState extends State<WeakNavigator> with TickerProviderStateMixin {
-  final List<Tuple4<OverlayEntry, AnimationController, AnimationController, Completer>> stack = [];
+  final List<Tuple5<OverlayEntry, AnimationController, CurvedAnimation, AnimationController, Completer>> stack = [];
   final _overlayKey = GlobalKey<OverlayState>();
   late OverlayEntry rootEntry;
-  late final AnimationController rootCoverAnimationController = AnimationController(vsync: this, duration: const Duration(milliseconds: 150));
+  late final AnimationController rootCoverAnimationController = AnimationController(vsync: this, duration: widget.duration);
 
   @override
   void initState() {
@@ -64,6 +68,7 @@ class WeakNavigatorState extends State<WeakNavigator> with TickerProviderStateMi
     rootEntry = OverlayEntry(
       builder: (context) => _AnimatedBlur(
         animation: rootCoverAnimationController,
+        curve: widget.curve,
         child: widget.child,
       ),
       opaque: true,
@@ -90,52 +95,58 @@ class WeakNavigatorState extends State<WeakNavigator> with TickerProviderStateMi
     );
   }
 
-  Future<T?> push<T extends Object?>(Widget widget) {
+  Future<T?> push<T extends Object?>(Widget child) {
     lightHapticFeedback();
     final forwardController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 150)
+      duration: widget.duration
+    );
+    final forwardCurvedAnimation = CurvedAnimation(
+      curve: widget.curve,
+      parent: forwardController
     );
     final coverController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 150)
+      duration: widget.duration
     );
-    final entry = Tuple4(OverlayEntry(
+    final entry = Tuple5(OverlayEntry(
       builder: (context) => FadeTransition(
-        opacity: forwardController,
+        opacity: forwardCurvedAnimation,
         child: _AnimatedBlur(
           animation: coverController,
-          child: widget
+          curve: widget.curve,
+          child: child
         )
       ),
       maintainState: true
-    ), forwardController, coverController, Completer<T>());
+    ), forwardController, forwardCurvedAnimation, coverController, Completer<T>());
     stack.add(entry);
     _overlayKey.currentState!.insert(entry.item1);
     forwardController.forward();
     if (stack.length > 1) {
-      stack[stack.length - 2].item3.forward();
+      stack[stack.length - 2].item4.forward();
     }
     else {
       rootCoverAnimationController.forward();
     }
-    return entry.item4.future;
+    return entry.item5.future;
   }
 
   void pop<T extends Object?>([T? result]) async {
     lightHapticFeedback();
     final entry = stack.removeLast();
     if (stack.isNotEmpty) {
-      stack.last.item3.reverse();
+      stack.last.item4.reverse();
     }
     else {
       rootCoverAnimationController.reverse();
     }
     await entry.item2.reverse(from: 1).orCancel;
     entry.item1.remove();
-    entry.item4.complete(result);
+    entry.item5.complete(result);
     entry.item2.dispose();
     entry.item3.dispose();
+    entry.item4.dispose();
   }
 
   Future<void> popAllExceptFirst({bool animated = false}) async {
@@ -146,9 +157,10 @@ class WeakNavigatorState extends State<WeakNavigator> with TickerProviderStateMi
     ]);
     for (final x in stack) {
       x.item1.remove();
-      x.item4.complete();
+      x.item5.complete();
       x.item2.dispose();
       x.item3.dispose();
+      x.item4.dispose();
     }
     stack.clear();
   }
@@ -162,10 +174,12 @@ class WeakNavigatorState extends State<WeakNavigator> with TickerProviderStateMi
 
 class _AnimatedBlur extends StatelessWidget {
   final AnimationController animation;
+  final Curve curve;
   final Widget child;
 
   const _AnimatedBlur({
     required this.animation,
+    this.curve = Curves.linear,
     required this.child,
     Key? key
   }) : super(key: key);
@@ -177,8 +191,8 @@ class _AnimatedBlur extends StatelessWidget {
       builder: (context, child) => ImageFiltered(
         enabled: animation.value != 0.0,
         imageFilter: ImageFilter.blur(
-          sigmaX: 5.0 * animation.value,
-          sigmaY: 5.0 * animation.value
+          sigmaX: 5.0 * curve.transform(animation.value),
+          sigmaY: 5.0 * curve.transform(animation.value)
         ),
         child: child
       ),
