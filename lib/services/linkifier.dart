a45587d438147eecb3e70631b9d5baa091b0c118
Callum Moffat <smartercallum@gmail.com>
2023-02-06 01:02:45 -0500

    Recognize 1-character domain names (t.me)

--- lib/services/linkifier.dart
+++ lib/services/linkifier.dart
@@ -1,7 +1,7 @@
 import 'package:linkify/linkify.dart';
 
 final _looseUrlRegex = RegExp(
-  r'^(.*?)((https?:\/\/)?(www\.)?([-a-zA-Z0-9@:%_.\+~#=]{2,256}\.[a-z]{2,})\b[-a-zA-Z0-9@:%_\+.~#?&//=]*)',
+  r'^(.*?)((https?:\/\/)?(www\.)?([-a-zA-Z0-9@:%_.\+~#=]{1,256}\.[a-z]{2,})\b[-a-zA-Z0-9@:%_\+.~#?&//=]*)',
   caseSensitive: false,
   dotAll: true,
 );
