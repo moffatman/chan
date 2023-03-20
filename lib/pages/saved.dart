b93acf5475872043ca5d4035cc0ed51e39043887
Callum Moffat <smartercallum@gmail.com>
2023-03-20 15:17:45 -0400

    Use dimReadThreads instead of Opacity for watches

--- lib/pages/saved.dart
+++ lib/pages/saved.dart
@@ -301,39 +301,37 @@ class _SavedPageState extends State<SavedPage> {
 																		return Text('missing for $threadState ${watch.item.threadIdentifier}');
 																	}
 																	else {
-																		return Opacity(
-																			opacity: watch.item.zombie ? 0.5 : 1.0,
-																			child: ThreadRow(
-																				thread: threadState!.thread!,
-																				isSelected: isSelected,
-																				showBoardName: true,
-																				showSiteIcon: true,
-																				onThumbnailLoadError: (error, stackTrace) {
-																					watch.imageboard.threadWatcher.fixBrokenThread(watch.item.threadIdentifier);
-																				},
-																				semanticParentIds: const [-4],
-																				onThumbnailTap: (initialAttachment) {
-																					final attachments = {
-																						for (final w in _watchedListController.items)
-																							for (final attachment in w.item.imageboard.persistence.getThreadStateIfExists(w.item.item.threadIdentifier)?.thread?.attachments ?? <Attachment>[])
-																								attachment: w.item.imageboard.persistence.getThreadStateIfExists(w.item.item.threadIdentifier)!
-																						};
-																					showGallery(
-																						context: context,
-																						attachments: attachments.keys.toList(),
-																						replyCounts: {
-																							for (final item in attachments.entries) item.key: item.value.thread!.replyCount
-																						},
-																						initialAttachment: attachments.keys.firstWhere((a) => a.id == initialAttachment.id),
-																						onChange: (attachment) {
-																							final threadId = attachments.entries.firstWhere((_) => _.key.id == attachment.id).value.identifier;
-																							_watchedListController.animateTo((p) => p.item.threadIdentifier == threadId);
-																						},
-																						semanticParentIds: [-4],
-																						heroOtherEndIsBoxFitCover: false
-																					);
-																				}
-																			)
+																		return ThreadRow(
+																			thread: threadState!.thread!,
+																			isSelected: isSelected,
+																			showBoardName: true,
+																			showSiteIcon: true,
+																			dimReadThreads: watch.item.zombie,
+																			onThumbnailLoadError: (error, stackTrace) {
+																				watch.imageboard.threadWatcher.fixBrokenThread(watch.item.threadIdentifier);
+																			},
+																			semanticParentIds: const [-4],
+																			onThumbnailTap: (initialAttachment) {
+																				final attachments = {
+																					for (final w in _watchedListController.items)
+																						for (final attachment in w.item.imageboard.persistence.getThreadStateIfExists(w.item.item.threadIdentifier)?.thread?.attachments ?? <Attachment>[])
+																							attachment: w.item.imageboard.persistence.getThreadStateIfExists(w.item.item.threadIdentifier)!
+																					};
+																				showGallery(
+																					context: context,
+																					attachments: attachments.keys.toList(),
+																					replyCounts: {
+																						for (final item in attachments.entries) item.key: item.value.thread!.replyCount
+																					},
+																					initialAttachment: attachments.keys.firstWhere((a) => a.id == initialAttachment.id),
+																					onChange: (attachment) {
+																						final threadId = attachments.entries.firstWhere((_) => _.key.id == attachment.id).value.identifier;
+																						_watchedListController.animateTo((p) => p.item.threadIdentifier == threadId);
+																					},
+																					semanticParentIds: [-4],
+																					heroOtherEndIsBoxFitCover: false
+																				);
+																			}
 																		);
 																	}
 																}
