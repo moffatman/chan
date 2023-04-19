eeef9f19a60282ecfdd9c48abc6ff8ba4e8a7403
Callum Moffat <smartercallum@gmail.com>
2023-04-18 22:53:57 -0400

    Layout and spacing fixes

--- lib/widgets/post_row.dart
+++ lib/widgets/post_row.dart
@@ -324,8 +324,8 @@ class PostRow extends StatelessWidget {
 								onPressed: () {
 									onThumbnailTap?.call(attachment);
 								}
-							)
-						)).expand((x) => [x, const SizedBox(height: 8)])
+							))
+						).expand((x) => [const SizedBox(height: 8), x]).skip(1)
 					]
 				)
 			);
@@ -363,9 +363,9 @@ class PostRow extends StatelessWidget {
 							TextSpan(
 								children: [
 									if (attachments != null) WidgetSpan(
-										child: IntrinsicWidth(child: attachments),
+										child: attachments,
 										floating: PlaceholderFloating.left,
-										alignment: PlaceholderAlignment.top
+										alignment: PlaceholderAlignment.middle
 									),
 									if (
 										// The site uses parentIds
@@ -413,11 +413,9 @@ class PostRow extends StatelessWidget {
 									),
 									const TextSpan(text: '\n'),
 									// In practice this is the height of a line of text
-									if (!shrinkWrap) const WidgetSpan(
-										child: SizedBox(
-											width: double.infinity,
-											height: 0
-										)
+									const WidgetSpan(
+										child: SizedBox.shrink(),
+										floating: PlaceholderFloating.left
 									)
 								]
 							),
