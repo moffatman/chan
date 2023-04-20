667cd2db33af9280f3a25a8217da11c1714756a3
Callum Moffat <smartercallum@gmail.com>
2023-04-20 02:15:03 -0400

    Floating placeholder polish
    - ClipRect + CupertinoButton for dev thread
    - Implement settings.imagesOnRight
    - Restore attachment minHeight (now 51 instead of 75)
    - Disable onThumbnailTap if no cb passed
    - Implement options.ignorePointer
    - Use PlaceholderFloating for reply count / counters placeholder
    - Adjust (increase) padding below attachment

--- lib/widgets/post_row.dart
+++ lib/widgets/post_row.dart
@@ -321,7 +321,7 @@ class PostRow extends StatelessWidget {
 		final Widget? attachments;
 		if (smallAttachments.isNotEmpty && settings.showImages(context, latestPost.board)) {
 			attachments = Padding(
-				padding: (settings.imagesOnRight && replyIds.isNotEmpty) ? const EdgeInsets.only(bottom: 32, right: 8) : const EdgeInsets.only(right: 8),
+				padding: settings.imagesOnRight ? const EdgeInsets.only(left: 8) : const EdgeInsets.only(right: 8),
 				child: Column(
 					crossAxisAlignment: CrossAxisAlignment.start,
 					mainAxisSize: MainAxisSize.min,
@@ -334,7 +334,7 @@ class PostRow extends StatelessWidget {
 								onPressed: bind1(onThumbnailTap, attachment),
 								child: ConstrainedBox(
 									constraints: const BoxConstraints(
-										//minHeight: 75
+										minHeight: 51
 									),
 									child: AttachmentThumbnail(
 										attachment: attachment,
@@ -355,8 +355,8 @@ class PostRow extends StatelessWidget {
 										)
 									)
 								)
-							)
-						)).expand((x) => [const SizedBox(height: 8), x]).skip(1).toList()
+							))
+						).expand((x) => [x, const SizedBox(height: 8)])
 					]
 				)
 			);
@@ -404,7 +404,7 @@ class PostRow extends StatelessWidget {
 								children: [
 									if (attachments != null) WidgetSpan(
 										child: attachments,
-										floating: PlaceholderFloating.left,
+										floating: settings.imagesOnRight ? PlaceholderFloating.right : PlaceholderFloating.left,
 										alignment: PlaceholderAlignment.middle
 									),
 									if (
@@ -450,15 +450,15 @@ class PostRow extends StatelessWidget {
 													)
 												)
 											) : ((settings.cloverStyleRepliesButton || replyIds.isEmpty) ? null : WidgetSpan(
-												child: SizedBox(width: (4 + replyIds.length.toString().length) * 8)
+												child: SizedBox(width: (4 + replyIds.length.toString().length) * 8),
+												floating: PlaceholderFloating.right
 											))) : null
 										)
 									),
 									const TextSpan(text: '\n'),
 									// In practice this is the height of a line of text
 									const WidgetSpan(
-										child: SizedBox.shrink(),
-										floating: PlaceholderFloating.left
+										child: SizedBox.shrink()
 									)
 								]
 							),
@@ -624,7 +624,7 @@ class PostRow extends StatelessWidget {
 												crossAxisAlignment: CrossAxisAlignment.start,
 												mainAxisAlignment: MainAxisAlignment.start,
 												mainAxisSize: MainAxisSize.min,
-												children: settings.imagesOnRight ? mainRow.reversed.toList() : mainRow
+												children: mainRow
 											)
 										),
 										if (cloverStyleRepliesButton) SizedBox(
