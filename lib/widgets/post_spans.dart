4ec88c7a5d3f377facb10bdc10a08097a3bd9905
Callum Moffat <smartercallum@gmail.com>
2023-04-22 03:03:04 -0400

    Fix options.ignorePointer for embed buttons

--- lib/widgets/post_spans.dart
+++ lib/widgets/post_spans.dart
@@ -1256,9 +1256,9 @@ class PostLinkSpan extends PostSpan {
 							)
 						);
 					}
-					onTap() {
+					final onTap = options.ignorePointer ? null : () {
 						openBrowser(context, cleanedUri!);
-					}
+					};
 					return WidgetSpan(
 						alignment: PlaceholderAlignment.middle,
 						child: CupertinoButton(
