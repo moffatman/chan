a7fed9a47ddcd791d3cb9b1fca18a2442df1da03
Callum Moffat <smartercallum@gmail.com>
2023-04-22 03:03:04 -0400

    Fix options.ignorePointer for embed buttons

--- lib/widgets/post_spans.dart
+++ lib/widgets/post_spans.dart
@@ -1044,9 +1044,9 @@ class PostLinkSpan extends PostSpan {
 							)
 						);
 					}
-					onTap() {
+					final onTap = options.ignorePointer ? null : () {
 						openBrowser(context, cleanedUri!);
-					}
+					};
 					return WidgetSpan(
 						alignment: PlaceholderAlignment.middle,
 						child: options.avoidBuggyClippers ? GestureDetector(
