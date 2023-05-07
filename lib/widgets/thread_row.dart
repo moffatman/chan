68dbd92f4b40b372f8b7a09e52669493c3cec17f
Callum Moffat <smartercallum@gmail.com>
2023-05-07 03:16:23 -0400

    Disable TextOverflow.fade for now

--- lib/widgets/thread_row.dart
+++ lib/widgets/thread_row.dart
@@ -479,7 +479,7 @@ class ThreadRow extends StatelessWidget {
 											]
 										]
 									),
-									overflow: TextOverflow.fade
+									//overflow: TextOverflow.fade
 								)
 							)
 						)
