18d90931fd76e0072d5c22450631d2a78060820b
Callum Moffat <smartercallum@gmail.com>
2023-05-07 23:00:22 -0400

    Fix Hero destination rect for widgetspan

--- lib/widgets/attachment_thumbnail.dart
+++ lib/widgets/attachment_thumbnail.dart
@@ -135,6 +135,7 @@ class AttachmentThumbnail extends StatelessWidget {
 	final bool mayObscure;
 	final AttachmentThumbnailCornerIcon? cornerIcon;
 	final bool expand;
+	final double heroScale;
 
 	const AttachmentThumbnail({
 		required this.attachment,
@@ -154,6 +155,7 @@ class AttachmentThumbnail extends StatelessWidget {
 		this.cornerIcon,
 		this.expand = false,
 		required this.mayObscure,
+		this.heroScale = 1.0,
 		Key? key
 	}) : super(key: key);
 
@@ -178,6 +180,7 @@ class AttachmentThumbnail extends StatelessWidget {
 						final fittedStartSize = applyBoxFit(BoxFit.contain, Size(attachment.width!.toDouble(), attachment.height!.toDouble()), startRect.size).destination;
 						startRect = Alignment.center.inscribe(fittedStartSize, startRect);
 					}
+					endRect = Rect.fromLTWH(endRect.left, endRect.top, endRect.width * heroScale, endRect.height * heroScale);
 				}
 				return CurvedRectTween(curve: Curves.ease, begin: startRect, end: endRect);
 			}
