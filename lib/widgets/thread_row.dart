4a17023d48aa9035f8bfff3c11a7867db3c696ef
Callum Moffat <smartercallum@gmail.com>
2023-05-07 23:00:22 -0400

    Fix Hero destination rect for widgetspan

--- lib/widgets/thread_row.dart
+++ lib/widgets/thread_row.dart
@@ -618,7 +618,8 @@ class ThreadRow extends StatelessWidget {
 												backgroundColor: backgroundColor,
 												borderColor: borderColor,
 												size: null
-											)
+											),
+											heroScale: MediaQuery.textScaleFactorOf(context)
 										)
 									)
 								)
