328b748160618b505bff12083bdc03b6f8afc89d
Callum Moffat <smartercallum@gmail.com>
2023-05-11 20:40:38 -0400

    Fix too wide attachmentsDeleted button

--- lib/widgets/post_row.dart
+++ lib/widgets/post_row.dart
@@ -338,14 +338,12 @@ class PostRow extends StatelessWidget {
 			);
 		}
 		else if (latestPost.attachmentDeleted) {
-			attachments = Center(
-				child: SizedBox(
-					width: 75,
-					height: 75,
-					child: CupertinoButton(
-						onPressed: onRequestArchive,
-						child: const Icon(CupertinoIcons.xmark_square, size: 36)
-					)
+			attachments = SizedBox(
+				width: 75,
+				height: 75,
+				child: CupertinoButton(
+					onPressed: onRequestArchive,
+					child: const Icon(CupertinoIcons.xmark_square, size: 36)
 				)
 			);
 		}
