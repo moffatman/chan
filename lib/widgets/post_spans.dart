dffe96ce4e710a5b2e7bdcbc2302ba7b6dc22b60
Callum Moffat <smartercallum@gmail.com>
2023-06-05 02:35:44 -0400

    Fix alignment of thumbnail with tall codespan

--- lib/widgets/post_spans.dart
+++ lib/widgets/post_spans.dart
@@ -138,6 +138,7 @@ abstract class PostSpan {
 	String buildText();
 	double estimateLines(double charactersPerLine) => buildText().length / charactersPerLine;
 	bool get containsLink => false;
+	bool get hasVeryTallWidgetSpan => false;
 }
 
 class _PostWrapperSpan extends PostSpan {
@@ -268,6 +269,16 @@ class PostNodeSpan extends PostSpan {
 
 	@override
 	bool get containsLink => children.any((c) => c.containsLink);
+
+	@override
+	bool get hasVeryTallWidgetSpan {
+		for (final child in children) {
+			if (child.hasVeryTallWidgetSpan) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
 
 class PostTextSpan extends PostSpan {
@@ -862,6 +873,12 @@ class PostCodeSpan extends PostSpan {
 	@override
 	String buildText() {
 		return '[code]$text[/code]';
+	}
+
+	@override
+	bool get hasVeryTallWidgetSpan {
+		final lineCount = RegExp(r'\n').allMatches(text).length + 1;
+		return lineCount > 5;
 	}
 }
 
