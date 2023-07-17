9b08f195880b871315c6b719ae4e868806bdef7c
Callum Moffat <smartercallum@gmail.com>
2023-07-17 03:12:14 -0400

    imagesOnRight fix for !classicCatalogLayout
    Swap side of counters when imagesOnRight

--- lib/widgets/thread_row.dart
+++ lib/widgets/thread_row.dart
@@ -560,7 +560,7 @@ class ThreadRow extends StatelessWidget {
 			}
 			return [
 				const SizedBox(width: 8),
-				if (!site.classicCatalogStyle && attachments != null) Padding(
+				if (!settings.imagesOnRight && !site.classicCatalogStyle && attachments != null) Padding(
 					padding: const EdgeInsets.only(top: 8),
 					child: attachments,
 				),
@@ -650,7 +650,11 @@ class ThreadRow extends StatelessWidget {
 							)
 						)
 					)
-				)
+				),
+				if (settings.imagesOnRight && !site.classicCatalogStyle && attachments != null) Padding(
+					padding: const EdgeInsets.only(top: 8, right: 8),
+					child: attachments,
+				),
 			];
 		}
 		Widget buildContentFocused() {
