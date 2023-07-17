caebb06ebaf1e6107b80ffa64f8f386e3937d3c3
Callum Moffat <smartercallum@gmail.com>
2023-07-16 20:33:55 -0400

    Proper icons for thumbs.chance.surf 1x1

--- lib/widgets/attachment_thumbnail.dart
+++ lib/widgets/attachment_thumbnail.dart
@@ -165,7 +165,7 @@ class AttachmentThumbnail extends StatelessWidget {
 							height: effectiveHeight,
 							color: barColor,
 							child: Center(
-								child: Icon(attachment.type == AttachmentType.url ? CupertinoIcons.compass : CupertinoIcons.exclamationmark_triangle_fill, size: max(24, 0.5 * min(effectiveWidth, effectiveHeight)))
+								child: Icon((loadstate.extendedImageLoadState == LoadState.failed ? CupertinoIcons.exclamationmark_triangle_fill : (attachment.icon ?? Adaptive.icons.photo)), size: max(24, 0.5 * min(effectiveWidth, effectiveHeight)))
 							)
 						);
 					}
