f0a0f5b86e2806e2d222877946faa186eb33d2ef
Callum Moffat <smartercallum@gmail.com>
2023-07-20 02:32:57 -0400

    Fix error on malformed soundpost filename

--- lib/services/soundposts.dart
+++ lib/services/soundposts.dart
@@ -6,11 +6,17 @@ extension SoundpostAttachment on Attachment {
 	Uri? get soundSource {
 		final match = _soundSourceRegex.firstMatch(filename);
 		if (match != null) {
-			final source = Uri.tryParse(Uri.decodeFull(match.group(1)!));
-			if (source?.hasScheme ?? false) {
-				return source;
+			try {
+				final source = Uri.tryParse(Uri.decodeFull(match.group(1)!));
+				if (source?.hasScheme ?? false) {
+					return source;
+				}
+				return source?.replace(scheme: 'https');
 			}
-			return source?.replace(scheme: 'https');
+			on ArgumentError {
+				// Bad URL encoding
+				return null;
+			}
 		}
 		return null;
 	}
