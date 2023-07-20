8909f10dfd05f9671213c2bb41a34492cb95cfc0
Callum Moffat <smartercallum@gmail.com>
2023-07-20 02:37:26 -0400

    Remove textScaleFactor heroScale
    After flutter/flutter upgrade, the WidgetSpan no longer needs this correction

--- lib/widgets/post_row.dart
+++ lib/widgets/post_row.dart
@@ -324,8 +324,7 @@ class PostRow extends StatelessWidget {
 											backgroundColor: theme.backgroundColor,
 											borderColor: theme.primaryColorWithBrightness(0.2),
 											size: null
-										),
-										heroScale: MediaQuery.textScaleFactorOf(context)
+										)
 									)
 								),
 								onPressed: () {
