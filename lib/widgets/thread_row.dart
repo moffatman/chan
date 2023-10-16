d7af67018579753b8973b9826fbed37864c27600
Callum Moffat <smartercallum@gmail.com>
2023-10-16 22:47:23 +0200

    Fixed-height URL thumb thread rows

--- lib/widgets/thread_row.dart
+++ lib/widgets/thread_row.dart
@@ -468,7 +468,7 @@ class ThreadRow extends StatelessWidget {
 								onPressed: onThumbnailTap == null ? null : () => onThumbnailTap?.call(attachment),
 								child: ConstrainedBox(
 									constraints: BoxConstraints(
-										minHeight: 51,
+										minHeight: attachment.type == AttachmentType.url ? 75 : 51,
 										maxHeight: attachment.type == AttachmentType.url ? 75 : double.infinity
 									),
 									child: AttachmentThumbnail(
