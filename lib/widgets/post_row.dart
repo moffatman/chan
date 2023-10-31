ff64232692fe5667329d44563a0a67952ffb781c
Callum Moffat <smartercallum@gmail.com>
2023-11-01 00:15:08 +0100

    Floating attachments cloverStyleRepliesButton
    Avoid trailing line from paragraph to keep consistent height below images

--- lib/widgets/post_row.dart
+++ lib/widgets/post_row.dart
@@ -415,11 +415,13 @@ class PostRow extends StatelessWidget {
 										))
 									)
 								),
-								const TextSpan(text: '\n'),
-								// In practice this is the height of a line of text
-								const WidgetSpan(
-									child: SizedBox.shrink()
-								)
+								if (!cloverStyleRepliesButton) ...[
+									const TextSpan(text: '\n'),
+									// In practice this is the height of a line of text
+									const WidgetSpan(
+										child: SizedBox.shrink()
+									)
+								]
 							]
 						),
 						overflow: TextOverflow.fade
@@ -549,7 +551,7 @@ class PostRow extends StatelessWidget {
 											)
 										),
 										if (cloverStyleRepliesButton) SizedBox(
-											height: 24 * settings.textScale
+											height: 48 * settings.textScale
 										)
 									]
 								)
