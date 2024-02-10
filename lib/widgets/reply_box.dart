733e20c5dd792390067d15a34e8546e859d34ee1
Callum Moffat <smartercallum@gmail.com>
2024-02-09 20:07:44 -0500

    Accept mpeg file

--- lib/widgets/reply_box.dart
+++ lib/widgets/reply_box.dart
@@ -774,7 +774,7 @@ Future<void> _handleImagePaste({bool manual = true}) async {
 					showToastIfOnlyRandomizingChecksum: forceRandomizeChecksum
 				);
 			}
-			else if (ext == 'mp4' || ext == 'mov' || ext == 'm4v' || ext == 'mkv') {
+			else if (ext == 'mp4' || ext == 'mov' || ext == 'm4v' || ext == 'mkv' || ext == 'mpeg') {
 				file = await _showTranscodeWindow(
 					source: file,
 					audioAllowed: board.webmAudioAllowed,
