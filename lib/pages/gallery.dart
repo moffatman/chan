5b8d19bf40ec1e49882150b0edcf1e5af2e4b9f6
Callum Moffat <smartercallum@gmail.com>
2024-09-09 00:00:03 -0400

    Cleanup "jump to attachment"
    - Actually jump with milliseconds=0. animation could freeze
    - Make the popup barrierDismissible
    - Make the button untappable when hidden

--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -1239,59 +1239,63 @@ class _GalleryPageState extends State<GalleryPage> {
 											animation: Listenable.merge([_shouldShowPosition, _currentAttachmentChanged]),
 											builder: (context, _) => Align(
 												alignment: Alignment.bottomLeft,
-												child: AnimatedOpacity(
-													duration: const Duration(milliseconds: 300),
-													opacity: showChrome || (_shouldShowPosition.value && settings.showOverlaysInGallery) ? 1 : 0,
-													child: GestureDetector(
-														onTap: () async {
-															final controller = TextEditingController();
-															final str = await showAdaptiveDialog<String>(
-																context: context,
-																builder: (context) => AdaptiveAlertDialog(
-																	title: const Text('Jump to Attachment'),
-																	content: AdaptiveTextField(
-																		autofocus: true,
-																		controller: controller,
-																		keyboardType: TextInputType.number,
-																		placeholder: 'Attachment #',
-																		onSubmitted: (s) => Navigator.pop(context, s),
-																	),
-																	actions: [
-																		AdaptiveDialogAction(
-																			onPressed: () => Navigator.pop(context, controller.value),
-																			child: const Text('Go')
+												child: IgnorePointer(
+													ignoring: !(showChrome || (_shouldShowPosition.value && settings.showOverlaysInGallery)),
+													child: AnimatedOpacity(
+														duration: const Duration(milliseconds: 300),
+														opacity: showChrome || (_shouldShowPosition.value && settings.showOverlaysInGallery) ? 1 : 0,
+														child: GestureDetector(
+															onTap: () async {
+																final controller = TextEditingController();
+																final str = await showAdaptiveDialog<String>(
+																	barrierDismissible: true,
+																	context: context,
+																	builder: (context) => AdaptiveAlertDialog(
+																		title: const Text('Jump to Attachment'),
+																		content: AdaptiveTextField(
+																			autofocus: true,
+																			controller: controller,
+																			keyboardType: TextInputType.number,
+																			placeholder: 'Attachment #',
+																			onSubmitted: (s) => Navigator.pop(context, s),
 																		),
-																		AdaptiveDialogAction(
-																			onPressed: () => Navigator.pop(context),
-																			child: const Text('Cancel')
-																		)
-																	]
+																		actions: [
+																			AdaptiveDialogAction(
+																				onPressed: () => Navigator.pop(context, controller.value),
+																				child: const Text('Go')
+																			),
+																			AdaptiveDialogAction(
+																				onPressed: () => Navigator.pop(context),
+																				child: const Text('Cancel')
+																			)
+																		]
+																	)
+																);
+																controller.dispose();
+																if (!context.mounted) {
+																	return;
+																}
+																final index = int.tryParse(str ?? '');
+																if (index != null) {
+																	_animateToPage((index - 1).clamp(0, widget.attachments.length - 1), milliseconds: 0);
+																}
+															},
+															child: Container(
+																margin: showChrome ? EdgeInsets.only(
+																	bottom: (settings.showThumbnailsInGallery ? MediaQuery.sizeOf(context).height * 0.2 : (44 + MediaQuery.paddingOf(context).bottom)) + 16 - (currentController.videoPlayerController == null ? 44 : 0),
+																	left: 16
+																) : const EdgeInsets.all(16),
+																padding: const EdgeInsets.all(8),
+																decoration: const BoxDecoration(
+																	borderRadius: BorderRadius.all(Radius.circular(8)),
+																	color: Colors.black54
+																),
+																child: AnimatedBuilder(
+																	animation: _currentAttachmentChanged,
+																	builder: (context, _) => Text("${currentIndex + 1} / ${widget.attachments.length}", style: TextStyle(
+																		color: settings.darkTheme.primaryColor
+																	))
 																)
-															);
-															controller.dispose();
-															if (!context.mounted) {
-																return;
-															}
-															final index = int.tryParse(str ?? '');
-															if (index != null) {
-																_animateToPage((index - 1).clamp(0, widget.attachments.length - 1));
-															}
-														},
-														child: Container(
-															margin: showChrome ? EdgeInsets.only(
-																bottom: (settings.showThumbnailsInGallery ? MediaQuery.sizeOf(context).height * 0.2 : (44 + MediaQuery.paddingOf(context).bottom)) + 16 - (currentController.videoPlayerController == null ? 44 : 0),
-																left: 16
-															) : const EdgeInsets.all(16),
-															padding: const EdgeInsets.all(8),
-															decoration: const BoxDecoration(
-																borderRadius: BorderRadius.all(Radius.circular(8)),
-																color: Colors.black54
-															),
-															child: AnimatedBuilder(
-																animation: _currentAttachmentChanged,
-																builder: (context, _) => Text("${currentIndex + 1} / ${widget.attachments.length}", style: TextStyle(
-																	color: settings.darkTheme.primaryColor
-																))
 															)
 														)
 													)
