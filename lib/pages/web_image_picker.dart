1aa2a15321267469ee6a86cdfd8d8b0bdec4dba1
Callum Moffat <smartercallum@gmail.com>
2024-10-06 22:53:30 +0200

    Fix bad context.select in web-image-picker

--- lib/pages/web_image_picker.dart
+++ lib/pages/web_image_picker.dart
@@ -581,7 +581,7 @@ class _WebImagePickerPageState extends State<WebImagePickerPage> {
 																			builder: (innerContext) => OverscrollModalPage(
 																				child: Container(
 																					width: double.infinity,
-																					color: ChanceTheme.backgroundColorOf(context),
+																					color: ChanceTheme.backgroundColorOf(innerContext),
 																					padding: const EdgeInsets.all(16),
 																					child: Column(
 																						children: [
