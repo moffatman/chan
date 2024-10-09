882941738263260ccacf1a6fe9e376efccfd070e
Callum Moffat <smartercallum@gmail.com>
2024-10-10 00:46:13 +0200

    Cleanup imagesOnRight / countersPlaceholder
    - Use width=double.infinity for contentFocused to avoid line break height
    - Fix missing left padding
    - Fix !classicCatalogStyle + imagesOnRight

--- lib/widgets/thread_row.dart
+++ lib/widgets/thread_row.dart
@@ -591,7 +591,7 @@ class ThreadRow extends StatelessWidget {
 			}
 			return [
 				const SizedBox(width: 8),
-				if (!settings.imagesOnRight && !site.classicCatalogStyle && attachments != null) Padding(
+				if (!site.classicCatalogStyle && attachments != null) Padding(
 					padding: const EdgeInsets.only(top: 8),
 					child: attachments,
 				),
@@ -630,7 +630,14 @@ class ThreadRow extends StatelessWidget {
 												postInject: settings.useFullWidthForCatalogCounters || (showLastReplies && thread.posts_.length > 1) ? null : countersPlaceholder
 											)
 										]
-										else if (!settings.useFullWidthForCatalogCounters && !(showLastReplies && thread.posts_.length > 1)) countersPlaceholder,
+										else if (!settings.useFullWidthForCatalogCounters && !(showLastReplies && thread.posts_.length > 1))
+											if (settings.imagesOnRight && !site.classicCatalogStyle) ...[
+												const TextSpan(text: '\n'),
+												WidgetSpan(
+													child: countersPlaceholderWidget
+												)
+											]
+											else countersPlaceholder,
 										// Hack to avoid extra line with same height of countersPlaceholder
 										const TextSpan(text: ' ', style: TextStyle(fontSize: 0)),
 										// Uuse thread and not latestThread
@@ -677,10 +684,7 @@ class ThreadRow extends StatelessWidget {
 						)
 					)
 				),
-				if (settings.imagesOnRight && !site.classicCatalogStyle && attachments != null) Padding(
-					padding: const EdgeInsets.only(top: 8, right: 8),
-					child: attachments,
-				),
+				if (settings.imagesOnRight) const SizedBox(width: 8)
 			];
 		}
 		Widget buildContentFocused() {
@@ -777,7 +781,12 @@ class ThreadRow extends StatelessWidget {
 							if (site.classicCatalogStyle && op.text.isNotEmpty) op.span.build(ctx, op, ctx.watch<PostSpanZoneData>(), settings, theme, (baseOptions ?? const PostSpanRenderOptions()).copyWith(
 								hideThumbnails: hideThumbnails
 							), constraints: spanConstraints),
-							if (!settings.useFullWidthForCatalogCounters && !settings.catalogGridModeTextAboveAttachment) countersPlaceholder,
+							if (!settings.useFullWidthForCatalogCounters && !settings.catalogGridModeTextAboveAttachment) WidgetSpan(
+								child: SizedBox(
+									width: double.infinity,
+									child: countersPlaceholderWidget
+								)
+							),
 							if (!settings.catalogGridModeAttachmentInBackground && !settings.catalogGridModeShowMoreImageIfLessText && style == ThreadRowStyle.grid) TextSpan(text: '\n' * 25)
 						];
 						return IgnorePointer(
