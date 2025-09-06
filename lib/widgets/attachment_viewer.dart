002738f90b794df21ec6f2596f1603a424f99495
Callum Moffat <smartercallum@gmail.com>
2025-09-06 00:14:40 -0400

    Reduce longPressDuration for new video gestures

--- lib/widgets/attachment_viewer.dart
+++ lib/widgets/attachment_viewer.dart
@@ -2233,6 +2233,7 @@ class AttachmentViewer extends StatelessWidget {
 								children: [
 									Expanded(
 										child: GestureDetector(
+											longPressDuration: const Duration(milliseconds: 300),
 											onLongPressStart: (x) {
 												lightHapticFeedback();
 												controller._videoPlayerController?.player.setRate(2);
@@ -2246,6 +2247,7 @@ class AttachmentViewer extends StatelessWidget {
 									Expanded(
 										flex: 3,
 										child: GestureDetector(
+											longPressDuration: const Duration(milliseconds: 300),
 											onLongPressStart: (x) {
 												lightHapticFeedback();
 												controller._playingBeforeLongPress = controller._videoPlayerController?.player.state.playing ?? false;
@@ -2263,6 +2265,7 @@ class AttachmentViewer extends StatelessWidget {
 									),
 									Expanded(
 										child: GestureDetector(
+											longPressDuration: const Duration(milliseconds: 300),
 											onLongPressStart: (x) {
 												lightHapticFeedback();
 												controller._videoPlayerController?.player.setRate(2);
