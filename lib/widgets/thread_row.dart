1e51f10bc08af348d487ecb5929a7e0e35fdbda2
Callum Moffat <smartercallum@gmail.com>
2025-12-02 01:50:20 -0500

    Fix RelativeTimeSpan too big
    Really don't understand why this is happening only on fork branch
    
    But this should fix it

--- lib/widgets/thread_row.dart
+++ lib/widgets/thread_row.dart
@@ -146,7 +146,7 @@ TextSpan buildThreadCounters({
 				if (latestThread.time.year < 2000) TextSpan(text: '—', style: TextStyle(color: otherMetadataColor))
 				else RelativeTimeSpan(latestThread.time, style: TextStyle(
 					color: otherMetadataColor,
-					fontSize: 18,
+					fontSize: 16,
 					fontFeatures: const [FontFeature.tabularFigures()]
 				)),
 			]
@@ -174,7 +174,7 @@ TextSpan buildThreadCounters({
 					thread.lastUpdatedTime ?? thread.posts_.tryLast?.time ?? thread.time,
 					style: TextStyle(
 						color: replyCountColor,
-						fontSize: 18,
+						fontSize: 16,
 						fontFeatures: const [FontFeature.tabularFigures()]
 					)
 				)
@@ -269,7 +269,7 @@ TextSpan buildThreadCounters({
 		parts.insert(0, space);
 		parts.add(space);
 	}
-	return TextSpan(children: parts);
+	return TextSpan(children: parts, style: const TextStyle(fontSize: 16));
 }
 
 class _ThreadCounters extends StatelessWidget {
