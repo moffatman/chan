1ae4b97872f30af94303bce82acce7e0878a0bab
Callum Moffat <smartercallum@gmail.com>
2025-12-20 18:08:17 -0500

    Offer archive.today on long press URL

--- lib/widgets/post_spans.dart
+++ lib/widgets/post_spans.dart
@@ -1957,7 +1957,15 @@ class PostLinkSpan extends PostTerminalSpan {
 								initiallyUseArchive: imageboardTarget?.$3,
 								activate: true
 							);
-						}
+						},
+					'Open archived': () => openBrowser(
+						context,
+						Uri.https('archive.today', '/', {
+							'run': '1',
+							'url': cleanedUrl.toString()
+						}),
+						fromShareOne: true
+					)
 				}
 			);
 		}
