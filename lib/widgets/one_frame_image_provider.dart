54465d95566043e54cb2dfb408172cbcb7149e90
Callum Moffat <smartercallum@gmail.com>
2026-03-13 02:10:04 -0400

    Synchronous OneFrameImageProvider
    Use best practices to provide key and image in same frame if possible

--- lib/widgets/one_frame_image_provider.dart
+++ lib/widgets/one_frame_image_provider.dart
@@ -1,11 +1,18 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
 
 /// To expose protected method
 class _OneFrameImageStreamCompleter extends OneFrameImageStreamCompleter {
 	_OneFrameImageStreamCompleter(super.image);
 	void _reportImageChunkEvent(ImageChunkEvent event)	=> reportImageChunkEvent(event);
+}
+
+class _SynchronousOneFrameImageStreamCompleter extends ImageStreamCompleter {
+	_SynchronousOneFrameImageStreamCompleter(ImageInfo image) {
+		setImage(image);
+	}
 }
 
 class WrappedOneFrameImageProviderKey<T> {
@@ -31,30 +38,67 @@ class OneFrameImageProvider<T extends Object> extends ImageProvider<WrappedOneFr
 
 	@override
 	Future<WrappedOneFrameImageProviderKey<T>> obtainKey(ImageConfiguration configuration) {
-		return imageProvider.obtainKey(configuration).then((k) => WrappedOneFrameImageProviderKey(k));
+		Completer<WrappedOneFrameImageProviderKey<T>>? completer;
+		// If the imageProvider.obtainKey future is synchronous, then we will be able to fill in result with
+		// a value before completer is initialized below.
+		SynchronousFuture<WrappedOneFrameImageProviderKey<T>>? result;
+		imageProvider.obtainKey(configuration).then((T key) {
+			if (completer == null) {
+				// This future has completed synchronously (completer was never assigned),
+				// so we can directly create the synchronous result to return.
+				result = SynchronousFuture<WrappedOneFrameImageProviderKey<T>>(
+					WrappedOneFrameImageProviderKey(key),
+				);
+			} else {
+				// This future did not synchronously complete.
+				completer.complete(WrappedOneFrameImageProviderKey(key));
+			}
+		});
+		if (result != null) {
+			return result!;
+		}
+		// If the code reaches here, it means the imageProvider.obtainKey was not
+		// completed sync, so we initialize the completer for completion later.
+		completer = Completer<WrappedOneFrameImageProviderKey<T>>();
+		return completer.future;
 	}
 
 	@override
 	ImageStreamCompleter loadImage(WrappedOneFrameImageProviderKey<T> key, ImageDecoderCallback decode) {
 		final nested = imageProvider.loadImage(key.key, decode);
-		final completer = Completer<ImageInfo>();
-		final out = _OneFrameImageStreamCompleter(completer.future);
+		Completer<ImageInfo>? completer;
+		void Function(ImageChunkEvent event)? reportImageChunkEventCb;
+		void reportImageChunkEvent(ImageChunkEvent event) {
+			reportImageChunkEventCb?.call(event);
+		}
+		ImageInfo? result;
 		ImageStreamListener? listener;
 		listener = ImageStreamListener(
 			(image, synchronousCall) {
-				completer.complete(image);
+				if (completer == null) {
+					result = image;
+				}
+				else {
+					completer.complete(image);
+				}
 				// Give outer listener a chance
 				Future.microtask(() => nested.removeListener(listener!));
 			},
-			onChunk: out._reportImageChunkEvent,
+			onChunk: reportImageChunkEvent,
 			onError: (e, st) {
-				if (!completer.isCompleted) {
+				if (!completer!.isCompleted) {
 					completer.completeError(e, st);
 					nested.removeListener(listener!);
 				}
 			}
 		);
 		nested.addListener(listener);
+		if (result != null) {
+			return _SynchronousOneFrameImageStreamCompleter(result!);
+		}
+		completer = Completer();
+		final out = _OneFrameImageStreamCompleter(completer.future);
+		reportImageChunkEventCb = out._reportImageChunkEvent;
 		return out;
 	}
 }
