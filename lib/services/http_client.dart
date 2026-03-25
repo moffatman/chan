127f76d1ab098aff0b27b3cd44b474a125fb2e4e
Callum Moffat <smartercallum@gmail.com>
2026-03-24 21:32:17 -0400

    Adaptive TLS, QUIC, HTTP/3

--- lib/services/http_client.dart
+++ lib/services/http_client.dart
@@ -2,6 +2,7 @@ import 'package:chan/services/bad_certificate.dart';
 import 'package:chan/services/tls.dart';
 import 'package:dio/adapter.dart';
 import 'package:dio_http2_adapter/dio_http2_adapter.dart';
+import 'package:dio_http2_adapter/dio_http3_adapter.dart';
 
 class MyHttpClientAdapter1 extends DefaultHttpClientAdapter {
 	MyHttpClientAdapter1() {
@@ -17,5 +18,24 @@ class MyHttpClientAdapter2 extends Http2Adapter {
 			setting.onBadCertificate = (cert) => badCertificateCallback(cert, url.host, url.port);
 			applyTlsSettings(setting);
 		}
+	));
+}
+
+class MyHttpClientAdapter3 extends Http3Adapter {
+	MyHttpClientAdapter3({
+		bool preferHttp3WithoutAltSvc = false
+	}) : super(Http3ConnectionManager(
+		tcpConnectionManager: ConnectionManager(
+			onClientCreate: (url, setting) {
+				setting.onBadCertificate = (cert) => badCertificateCallback(cert, url.host, url.port);
+				applyTlsSettings(setting);
+			}
+		),
+		onClientCreate: (url, setting) {
+			setting.onBadCertificate = (cert) => badCertificateCallback(cert, url.host, url.port);
+			applyTlsSettings3(setting);
+		},
+		useNativeUdp: true,
+		preferHttp3WithoutAltSvc: preferHttp3WithoutAltSvc
 	));
 }
