44b3bc927f9033f052593c343eb94d0f881630b0
Callum Moffat <smartercallum@gmail.com>
2026-08-08 19:48:04 -0400

    Add latest Let's Encrypt root to whitelist
    For testing with very old Android VMs

--- lib/services/bad_certificate.dart
+++ lib/services/bad_certificate.dart
@@ -6,7 +6,9 @@ final _sha1s = {
 	// wizchan.org expired "Wednesday, May 15, 2024 at 5:49:26 AM"
 	'a053375bfe84e8b748782c7cee15827a6af5a405',
 	// 8chan.moe expired "Sunday, August 3, 2025 at 3:16:37 PM"
-	'00abefd055f9a9c784ffdeabd1dcdd8fed741436'
+	'00abefd055f9a9c784ffdeabd1dcdd8fed741436',
+	// /C=US/O=ISRG/CN=Root YR
+	'ab9d0263244dd0326eb67015705a667e79cfe998'
 };
 
 bool badCertificateCallback(X509Certificate cert, String host, int port) {
