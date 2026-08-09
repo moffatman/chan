73fb5dfd4df4fa11b860239908d48c733f4fec8b
Callum Moffat <smartercallum@gmail.com>
2026-08-08 21:35:42 -0400

    Better reply button alignment for deletedStub

--- lib/widgets/post_row.dart
+++ lib/widgets/post_row.dart
@@ -842,7 +842,7 @@ class PostRow extends StatelessWidget {
 										alignment: Alignment.bottomRight,
 										child: CupertinoInkwell(
 											alignment: Alignment.bottomRight,
-											padding: const EdgeInsets.only(left: 8, bottom: 16, right: 16),
+											padding: isDeletedStub ? const EdgeInsets.only(left: 8, bottom: 8, right: 16) : const EdgeInsets.only(left: 8, bottom: 16, right: 16),
 											onPressed: openReplies,
 											child: Transform.scale(
 												alignment: Alignment.bottomRight,
